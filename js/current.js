f7a0fb848644e82685dd1425c22f081c8dfb995d
Nataliia Trofymchuk <64868442+trofyna@users.noreply.github.com>
2022-01-28 18:39:40 +0200

    ind+styles+read .me
    form=modal__form=js-contact-form=js-speaker-form
    
    send-to-modal div>=modal_form
    h3=form__title

--- js/current.js
+++ js/current.js
@@ -1,6 +1,6 @@
 (() => {
     document
-      .querySelector('.js-speaker-form', '.modal__form')
+      .querySelector('.js-speaker-form', '.form')
       .addEventListener('submit', e => {
         e.preventDefault();
         new FormData(e.currentTarget).forEach((value, name) =>
